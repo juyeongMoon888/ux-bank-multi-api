f8a8eecc1f5219d3e7a216b63098d112f0114fd3
juyeongMoon888 <whoor2828@gmail.com>
2025-08-13 15:38:54 +0900

    feat: BankMultiApiApplication 클래스 생성

---
+++ src/main/java/com/mybank/multibank/BankMultiApiApplication.java
@@ -0,0 +1,13 @@
+package com.mybank.multibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMultiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMultiApiApplication.class, args);
+	}
+
+}
