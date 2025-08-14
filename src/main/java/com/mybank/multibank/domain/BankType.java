caeb3931624bccf21681e6ffb13940038260fab8
juyeongMoon888 <whoor2828@gmail.com>
2025-08-14 23:41:33 +0900

    feat: 은행명과 계좌번호 prefix를 매핑하는 BankType enum 추가

---
+++ src/main/java/com/mybank/multibank/domain/BankType.java
@@ -0,0 +1,24 @@
+package com.mybank.multibank.domain;
+
+public enum BankType {
+    KB("국민은행", "110"),
+    SHINHAN("신한은행", "120"),
+    WOORI("우리은행", "333")
+    ;
+
+    private final String displayName;
+    private final String prefix;
+
+    BankType(String displayName, String prefix) {
+        this.displayName = displayName;
+        this.prefix = prefix;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+}
