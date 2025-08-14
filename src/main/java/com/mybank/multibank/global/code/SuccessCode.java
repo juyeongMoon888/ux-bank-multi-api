c6fa48e17c89c438f9370c500fbf0b39f293ad94
juyeongMoon888 <whoor2828@gmail.com>
2025-08-14 23:47:43 +0900

    feat: SuccessCode에 ACCOUNT_CREATED 응답 코드 추가

--- src/main/java/com/mybank/multibank/global/code/SuccessCode.java
+++ src/main/java/com/mybank/multibank/global/code/SuccessCode.java
@@ -3,7 +3,9 @@ package com.mybank.multibank.global.code;
 import org.springframework.http.HttpStatus;
 
 public enum SuccessCode implements BaseCode{
-    ACCOUNT_VERIFIED("success.account_verified", HttpStatus.OK);
+    ACCOUNT_VERIFIED("success.account_verified", HttpStatus.OK),
+    ACCOUNT_CREATED("success.account_created", HttpStatus.OK)
+    ;
     private final String messageKey;
     private final HttpStatus httpStatus;
 
