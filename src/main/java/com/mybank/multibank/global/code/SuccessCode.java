dd6e4a1bf677a68a6eb21744d4308f574911f210
juyeongMoon888 <whoor2828@gmail.com>
2025-08-14 22:05:40 +0900

    feat: SuccessCode enum에 BaseCode 구현 및 성공 코드 정의

---
+++ src/main/java/com/mybank/multibank/global/code/SuccessCode.java
@@ -0,0 +1,23 @@
+package com.mybank.multibank.global.code;
+
+import org.springframework.http.HttpStatus;
+
+public enum SuccessCode implements BaseCode{
+    ACCOUNT_VERIFIED("success.account_verified", HttpStatus.OK);
+    private final String messageKey;
+    private final HttpStatus httpStatus;
+
+    SuccessCode(String messageKey, HttpStatus httpStatus) {
+        this.messageKey = messageKey;
+        this.httpStatus = httpStatus;
+    }
+
+    @Override
+    public String getMessageKey() {
+        return messageKey;
+    }
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+}
