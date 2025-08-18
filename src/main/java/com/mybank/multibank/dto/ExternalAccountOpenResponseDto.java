9886667e12594d7fd29280c8a395b29a72eb6c98
juyeongMoon888 <whoor2828@gmail.com>
2025-08-18 11:30:09 +0900

    feat: 외부 계좌 개설 응답용 ExternalAccountOpenResponseDto추가

---
+++ src/main/java/com/mybank/multibank/dto/ExternalAccountOpenResponseDto.java
@@ -0,0 +1,17 @@
+package com.mybank.multibank.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@Builder
+public class ExternalAccountOpenResponseDto {
+    private final Long userId;
+    private final String bankType;
+    private final String accountNumber;
+    private final String accountName;
+    private final String externalAccountId;
+    private final String balance;
+}
