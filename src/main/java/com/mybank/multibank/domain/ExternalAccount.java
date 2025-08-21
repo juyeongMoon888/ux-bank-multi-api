6648c3e8edbf0f3699cbf6ad7c0f55a6afc6062c
juyeongMoon888 <whoor2828@gmail.com>
2025-08-21 22:29:47 +0900

    feat: Account 엔티티 status 필드 enum 매핑 설정

--- src/main/java/com/mybank/multibank/domain/ExternalAccount.java
+++ src/main/java/com/mybank/multibank/domain/ExternalAccount.java
@@ -1,18 +1,38 @@
 package com.mybank.multibank.domain;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 
+@Entity
+@Table(
+    name = "external_accounts",
+    uniqueConstraints = @UniqueConstraint(
+            name = "uk_bank_account",
+            columnNames = {"bank_type","account_number"}
+    ),
+    indexes = @Index(name="idx_external_account_bank_type", columnList="bank_type")
+)
+@Getter
+@Builder
 public class ExternalAccount {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
     private Long id;
 
     private String bankType;
-    private String bankName;
-    private String accountNumber;
+    private String accountName;
+    private Long userId;
     private String ownerName;
-    private String status;
+    private String birth;
+    private String phone;
+    private String accountNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="status")
+    private AccountStatus status;
+
     private Long balance;
 }
