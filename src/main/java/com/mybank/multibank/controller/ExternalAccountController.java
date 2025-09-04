5f2a73bf51973eb26ed89d48ce2c3d42e371c2c5
juyeongMoon888 <whoor2828@gmail.com>
2025-09-04 13:06:19 +0900

    feat: 계좌개설/공용 오퍼레이션 추가

--- src/main/java/com/mybank/multibank/controller/ExternalAccountController.java
+++ src/main/java/com/mybank/multibank/controller/ExternalAccountController.java
@@ -1,31 +1,63 @@
 package com.mybank.multibank.controller;
 
-import com.mybank.multibank.domain.ExternalAccount;
-import com.mybank.multibank.dto.ExternalAccountVerifyResponse;
+import com.mybank.multibank.domain.verification.VerificationResult;
+import com.mybank.multibank.dto.*;
 import com.mybank.multibank.global.ResponseUtil;
 import com.mybank.multibank.global.code.SuccessCode;
-import com.mybank.multibank.service.ExternalAccountService;
-import lombok.Getter;
+import com.mybank.multibank.service.AccountService;
+import com.mybank.multibank.service.ValidateService;
+import com.mybank.multibank.service.transfer.model.ExternalHandler;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+@Slf4j
 @RestController
-@RequestMapping("/api/account")
+@RequestMapping("/api/external-bank")
 @RequiredArgsConstructor
 public class ExternalAccountController {
-    private final ExternalAccountService accountService;
+    private final AccountService accountService;
     private final ResponseUtil responseUtil;
+    private final ValidateService validateService;
+    private final ExternalHandler handler;
 
-    @GetMapping("/validate")
-    public ResponseEntity<?> validateAccount(
-            @RequestParam String bankCode,
-            @RequestParam String accountNumber) {
-        ExternalAccountVerifyResponse ownerName = accountService.validateAccount(bankCode, accountNumber);
-        return responseUtil.buildResponse(SuccessCode.ACCOUNT_VERIFIED, HttpStatus.OK, ownerName);
+    @PostMapping("/open-account")
+    public ResponseEntity<?> createAccount(@RequestBody ExternalOpenAccountRequestDto dto) {
+        ExternalAccountOpenResponseDto account = accountService.createAccount(dto);
+
+        return responseUtil.buildResponse(SuccessCode.ACCOUNT_CREATED, HttpStatus.OK, account);
     }
+
+    @PostMapping("/transfer/verify")
+    public VerificationResult verifyAccount(@RequestBody AccountVerifyRequestDto dto) {
+        return accountService.validateAccount(dto);
+    }
+
+    @PostMapping("/deposit")
+    public ExAccDepositRes deposit(@RequestBody ExAccDepositReq req) {
+        validateService.validateDeposit(req);
+        return handler.deposit(req);
+    }
+
+    @PostMapping("/withdraw")
+    public ExAccWithdrawRes withdraw(@RequestBody ExAccWithdrawReq req) {
+        validateService.validateWithdraw(req);
+        return handler.withdraw(req);
+    }
+
+    @PostMapping("/confirm")
+    public ExAccConfirmRes confirm(@RequestBody ExAccConfirmReq req) {
+        validateService.validateConfirm(req);
+        return handler.confirm(req);
+    }
+
+    @PostMapping("/cancel")
+    public ExAccCancelRes cancel(@RequestBody ExAccCancelReq req) {
+        validateService.validateCancel(req);
+        return handler.cancel(req);
+    }
+
+
+
 }
