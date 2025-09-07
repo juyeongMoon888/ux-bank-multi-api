0423564a184ae10329447a3e43bea318f9d25155
juyeongMoon888 <whoor2828@gmail.com>
2025-09-07 10:54:27 +0900

    refactor(i18n): 메시지 키를 snake_case로 통일 - reject.account.closed → reject.account_closed

--- src/main/java/com/mybank/multibank/global/code/RejectCode.java
+++ src/main/java/com/mybank/multibank/global/code/RejectCode.java
@@ -3,7 +3,8 @@ package com.mybank.multibank.global.code;
 import org.springframework.http.HttpStatus;
 
 public enum RejectCode {
-    ACCOUNT_CLOSED("reject.account.closed", HttpStatus.OK),
+    ACCOUNT_CLOSED("reject.account_closed", HttpStatus.OK),
+    ACCOUNT_FROZEN("reject.account_frozen", HttpStatus.OK)
     ;
     private final String message;
     private final HttpStatus httpStatus;
