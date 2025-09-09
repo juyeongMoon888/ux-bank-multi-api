fde99a782b6bd8ef02bb55ac3ec78ba52471236d
juyeongMoon888 <whoor2828@gmail.com>
2025-09-09 19:55:16 +0900

    refactor: 불필요한 findByIdAndAccount_Owner_Id 함수 제거

--- src/main/java/com/mybank/multibank/domain/transaction/TransactionRepository.java
+++ src/main/java/com/mybank/multibank/domain/transaction/TransactionRepository.java
@@ -1,5 +1,6 @@
 package com.mybank.multibank.domain.transaction;
 
+import com.mybank.multibank.service.transfer.model.OperationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,8 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transactions, Long> {
-
-    Optional<Transactions> findByIdAndAccount_Owner_Id(Long transactionId, Long userId);
 
     Optional<Transactions> findByIdForUpdate(long exTxId);
 }
