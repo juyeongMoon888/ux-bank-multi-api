5c6e68eaca0876155a0eaa9ac60562e2df0b4c20
juyeongMoon888 <whoor2828@gmail.com>
2025-09-10 15:20:34 +0900

    chore: 불필요한 import 제거

--- src/main/java/com/mybank/multibank/service/AccountService.java
+++ src/main/java/com/mybank/multibank/service/AccountService.java
@@ -1,13 +1,9 @@
 package com.mybank.multibank.service;
 
-import com.mybank.multibank.application.command.TransactionCommandService;
-import com.mybank.multibank.application.query.AccountQueryService;
 import com.mybank.multibank.assembler.ExternalAccountAssembler;
 import com.mybank.multibank.domain.AccountStatus;
 import com.mybank.multibank.domain.BankType;
-import com.mybank.multibank.domain.TransactionStatus;
 import com.mybank.multibank.domain.account.Account;
-import com.mybank.multibank.domain.transaction.Transactions;
 import com.mybank.multibank.domain.verification.VerificationResult;
 import com.mybank.multibank.dto.AccountVerifyRequestDto;
 import com.mybank.multibank.dto.ExternalOpenAccountRequestDto;
@@ -15,7 +11,6 @@ import com.mybank.multibank.dto.ExternalAccountOpenResponseDto;
 import com.mybank.multibank.global.code.ErrorCode;
 import com.mybank.multibank.global.exception.user.CustomException;
 import com.mybank.multibank.domain.account.AccountRepository;
-import com.mybank.multibank.service.transfer.model.OperationType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -37,7 +32,6 @@ public class AccountService {
             throw new CustomException(ErrorCode.BANK_INVALID);
         }
 
-        //계좌번호 중복 시 재시도 로직 넣기
         Account account = Account.builder()
                 .bank(BankType.valueOf(dto.getBank()))
                 .accountName(dto.getAccountName())
