f7c66b27ac75110b5e9e80fc64a582a5bbb11cb6
juyeongMoon888 <whoor2828@gmail.com>
2025-09-11 15:57:29 +0900

    feat: ExAccWithdrawReq에 flow(String)필드 추가

---
+++ src/main/java/com/mybank/multibank/dto/ExAccWithdrawReq.java
@@ -0,0 +1,21 @@
+package com.mybank.multibank.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ExAccWithdrawReq {
+    private String toBank;
+    private String toAccountNumber;
+    private Long amount;
+    private String memo;
+    private String idempotencyKey;
+    private String fromBank;
+    private String fromAccountNumber;
+    private String flow;
+}
